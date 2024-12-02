8c4187c8a94bef5a5b2956271cb9a08e5d7a573d
Marco <humarco1310@gmail.com>
2024-12-03 00:39:01 +0100

    Initial Commit

---
+++ RestartApp/RestartAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartAppApp.swift
+//  RestartApp
+//
+//  Created by Marco Hu on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
