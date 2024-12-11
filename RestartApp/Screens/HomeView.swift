e5b5087d3ed1c0ccb264792b695c00efd49782af
Marco <humarco1310@gmail.com>
2024-12-11 20:14:16 +0100

    altro

--- RestartApp/Screens/HomeView.swift
+++ RestartApp/Screens/HomeView.swift
@@ -8,8 +8,66 @@
 import SwiftUI
 
 struct HomeView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive = false
+    @State private var isAnimating: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            //MARK: - HEADER
+            
+            Spacer()
+            
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .offset(y: isAnimating ? 35 : -35)
+                    .animation(.easeInOut(duration: 4).repeatForever(), value: isAnimating)
+            }//: ZStack
+            
+            
+            //MARK: - CENTER
+            
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundStyle(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            
+            //MARK: - FOOTER
+            
+            Spacer()
+            
+            Button {
+                
+                withAnimation {
+                    playSound(sound: "success", type: "m4a")
+                    isOnboardingViewActive = true
+                }
+                
+            } label: {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+            }//: BUTTON
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+
+        } //:VSTACK
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                isAnimating = true
+            }
+            
+        }
+        
     }
 }
 
