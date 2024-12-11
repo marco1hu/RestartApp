1798eaa8807f6d3f3a4def8527d6d19d9a404fda
Marco <humarco1310@gmail.com>
2024-12-11 20:13:59 +0100

    programma completo

---
+++ RestartApp/Screens/OnBoardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnBoardingView.swift
+//  RestartApp
+//
+//  Created by Marco Hu on 03/12/24.
+//
+
+import SwiftUI
+
+struct OnBoardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnBoardingView()
+}
---
+++ RestartApp/Utilities/AudioPlayer.swift
@@ -0,0 +1,8 @@
+//
+//  AudioPlayer.swift
+//  RestartApp
+//
+//  Created by Marco Hu on 11/12/24.
+//
+
+import Foundation
